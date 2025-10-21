8af1cd9b02ffc68236cf16ce2d3aa5596d18c946
Rayan <rayan-itani@outlook.com>
2025-10-21 17:59:42 +0200

    #1.2.0: Update verison + add migration scripts

--- BuildingManagement/Migrations/20251021155743_1.2.0_Update_UpdateAt.Designer.cs
+++ BuildingManagement/Migrations/20251021155743_1.2.0_Update_UpdateAt.Designer.cs
@@ -4,6 +4,7 @@ using BuildingManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BuildingManagement.Migrations
 {
     [DbContext(typeof(BuildingManagementContext))]
-    partial class BuildingManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20251021155743_1.2.0_Update_UpdateAt")]
+    partial class _120_Update_UpdateAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
