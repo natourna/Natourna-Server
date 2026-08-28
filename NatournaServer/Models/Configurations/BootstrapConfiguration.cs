0c1c2392d049dcba89783a64778aafc5765c8c14
IssaItani-iri00 <IssaItani-iri00@users.noreply.github.com>
2026-08-29 00:49:56 +0300

    added bootstrap admin configuration

---
+++ NatournaServer/Models/Configurations/BootstrapConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace NatournaServer.Models.Configurations
+{
+    public class BootstrapConfiguration
+    {
+        public string AdminEmail { get; set; } = string.Empty;
+
+        public string AdminPassword { get; set; } = string.Empty;
+    }
+}
