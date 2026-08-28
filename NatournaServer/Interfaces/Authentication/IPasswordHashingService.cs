a3abd5f26d1ae753235f5f8eaa4375603de32fc6
IssaItani-iri00 <IssaItani-iri00@users.noreply.github.com>
2026-08-29 00:47:55 +0300

    added a bcrypt password hashing service

---
+++ NatournaServer/Interfaces/Authentication/IPasswordHashingService.cs
@@ -0,0 +1,9 @@
+﻿namespace NatournaServer.Interfaces.Authentication
+{
+    public interface IPasswordHashingService
+    {
+        string HashPassword(string password);
+
+        bool VerifyPassword(string hashedPassword, string providedPassword);
+    }
+}
