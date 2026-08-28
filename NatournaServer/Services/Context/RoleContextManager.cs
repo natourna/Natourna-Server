15e47d63dc2e013edd32bdd08a283a6ce09448d9
IssaItani-iri00 <IssaItani-iri00@users.noreply.github.com>
2026-08-29 00:42:47 +0300

    seeded reference roles at startup

---
+++ NatournaServer/Services/Context/RoleContextManager.cs
@@ -0,0 +1,42 @@
+﻿using NatournaServer.Data;
+using NatournaServer.Interfaces.Context;
+using NatournaServer.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace NatournaServer.Services.Context
+{
+    public class RoleContextManager : IRoleContextManager
+    {
+        private readonly NatournaServerContext _context;
+
+        public RoleContextManager(NatournaServerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<RoleEntity>> GetAllAsync()
+        {
+            return await _context.Roles
+                .ToListAsync();
+        }
+
+        public async Task<RoleEntity?> GetByIdAsync(int id)
+        {
+            return await _context.Roles
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task<RoleEntity?> GetByNameAsync(string name)
+        {
+            return await _context.Roles
+                .FirstOrDefaultAsync(r => r.Name == name);
+        }
+
+        public async Task<RoleEntity> CreateAsync(RoleEntity role)
+        {
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+            return role;
+        }
+    }
+}
